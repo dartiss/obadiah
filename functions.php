5761766e5fe577a627926e5c6675bb076d05823f
David Artiss <david.artiss@automattic.com>
2019-08-08 15:36:12 +0100

    New functions and styles

--- functions.php
+++ functions.php
@@ -85,3 +85,41 @@ remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 add_filter( 'emoji_svg_url', '__return_false' );
+
+// Remove JQuery Migrate
+
+function remove_jquery_migrate( $scripts ) {
+
+	if ( !is_admin() && isset( $scripts->registered[ 'jquery' ] ) ) {
+		$script = $scripts->registered[ 'jquery' ];
+		
+		if ( $script->deps ) {
+			$script->deps = array_diff( $script->deps, array(
+				'jquery-migrate'
+			) );
+		}
+	}
+}
+
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+// Remove JS for embedding other WP posts
+
+function deregister_wp_embed() {
+
+	wp_deregister_script( 'wp-embed' );
+}
+
+add_action( 'wp_footer', 'deregister_wp_embed' );
+
+// Remove Windows Live Writer support
+
+remove_action( 'wp_head', 'wlwmanifest_link' );
+
+// Remove WordPress generator meta tag
+
+remove_action( 'wp_head', 'wp_generator' );
+
+// Remove feeds for categories, search, tags and post comments
+
+remove_action( 'wp_head', 'feed_links_extra', 3 );
